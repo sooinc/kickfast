dfc247c5e97f5ecb728cc49690f314012cb8d0ac
sooin.c <sooinc90@gmail.com>
2020-05-12 22:50:49 -0400

    updated component/client/index

--- client/components/index.js
+++ client/components/index.js
@@ -4,6 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as Login} from './Login'
-export {default as Signup} from './Signup'
+export {default as Login} from './login'
+export {default as Signup} from './signup'
 export {default as UserHome} from './user-home'
